ff66141bcda157929d05c4f01d289206ecd4ad5a
jrmensah <jerilynmensah@gmail.com>
2017-10-13 14:16:21 -0400

    Added display of multiples of 7,17,23 and user value

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,8 +7,9 @@ package com.company;
  * "Buzz" for multiples of 5, and "FizzBuzz" for multiples of 3 and 5.
  *
  * Update- October 13, 2017:
- * The user will now be asked to input a number greater than 1 and the program will display multiples
- * of that number, plus multiples of 7, 17 and 23 at the end of the program.
+ * The user will now be asked to input a number greater than 1 and count the number up to
+ * that number. The program will display the number of multiples of that number, plus
+ * multiples of 7, 17 and 23 at the end of the program.
  */
 
 import java.util.Scanner;
@@ -19,21 +20,20 @@ public class Main {
 
         int counter = 1;
         int currentValue = 0;
-        String answer;
         String FizzBuzz;
+
 
         Scanner keyboard = new Scanner(System.in);
 
-        do {
+
             System.out.println("Enter a number greater than 1");
             currentValue = keyboard.nextInt();
-            if(currentValue < 1)
-            {
+            if (currentValue < 1) {
                 System.out.println("Invalid Entry. Please enter a number greater than 1");
                 currentValue = keyboard.nextInt();
             }
 
-            for (counter =1; counter <= currentValue; counter++)
+            for (counter = 1; counter <= currentValue; counter++)
 
             {
                 FizzBuzz = "";
@@ -54,33 +54,45 @@ public class Main {
                 }
                 System.out.println(FizzBuzz);
 
-            }while(currentValue > 1);{
+            }
+                // Print the number of multiples of 7, 17, 23 and user input
+
+                System.out.printf( "The number of multiples of 7:   %d \n", +currentValue/7);
+                System.out.printf( "The number of multiples of 17:   %d \n", +currentValue/17);
+                System.out.printf( "The number of multiples of 23:   %d \n", +currentValue/23);
+                System.out.printf( "The number of multiples of user number:   %d \n", +currentValue/currentValue);
+
+
+
         }
-    }while(counter != currentValue);
-        {
-
-                //Print multiples of 7
-            if (counter % 7 == 0)
-            {
-                System.out.println(counter%7 == 0);
-            }
-                //Print multiples of 17
-            if (counter % 17 == 0)
-            {
-                System.out.println(counter%17 == 0);
-            }
-                //Print multiples of 23
-            if (counter % 23 == 0)
-            {
-                System.out.println(counter%23 == 0);
-
-            } //Print multiples of value the user entered
-            if (counter % currentValue == 0)
-            {
-                System.out.println(counter%currentValue == 0);
-            }
-
-        }System.out.print(counter!=currentValue);
-    }
     }
 
+//
+//                }
+//            }
+//
+//    }
+//
+//    }
+//
+//    }
+//}
+
+//            {
+////            //Print multiples of 7
+////            if (counter % 7 == 0) {
+////            System.out.println(counter % 7 == 0);
+////            }
+////            //Print multiples of 17
+////            if (counter % 17 == 0) {
+////            System.out.println(counter % 17 == 0);
+////            }
+////            //Print multiples of 23
+////            if (counter % 23 == 0) {
+////            System.out.println(counter % 23 == 0);
+////
+////            } //Print multiples of value the user entered
+////            if (counter % currentValue == 0) {
+////            System.out.println(counter % currentValue == 0);
+////            }
+////            }
